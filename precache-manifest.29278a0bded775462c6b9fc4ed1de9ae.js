fe91730f332216df7a56d2407d19940089bf49d2
Meagle <mindfulyeon@gmail.com>
2020-02-22 01:10:35 +0900

    Updates

---
+++ precache-manifest.29278a0bded775462c6b9fc4ed1de9ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4f887ac88356944da161f593456c5a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "98a48330d186ef3c89f6",
+    "url": "/movie_app_2020/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "56dd94ff12ce02272403",
+    "url": "/movie_app_2020/static/js/2.4ca24003.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_2020/static/js/2.4ca24003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a48330d186ef3c89f6",
+    "url": "/movie_app_2020/static/js/main.8b6c0265.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
